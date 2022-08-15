c376668ea18b1b86919c3c01a1cc2d114cefd748
Oskari Kinnunen <eino.oskari.kinnunen@gmail.com>
2022-08-15 06:50:41 +0300

    cors fix

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,4 @@
 module.exports = {
-	publicPath: process.env.NODE_ENV === "production" ? "/https://github.com/oskarikinnunen/vuesort/" : "/"
+	publicPath: process.env.NODE_ENV === "production" ? "/https://github.com/oskarikinnunen/vuesort/" : "/",
+	devServer: { proxy: 'https://github.com/oskarikinnunen/vuesort'}
 }
